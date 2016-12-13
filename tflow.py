484c64670e21f87882497f5fc50a0b21fc83afe6
Kamil Dydo <dydokamil@gmail.com>
2016-12-13 23:18:12 +0100

    Added distortion to images

--- tflow.py
+++ tflow.py
@@ -15,8 +15,7 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
-from Deep_Learning.kaggle.leaves.tools import crop_to_first, add_frame, distorted_input, load_images, batch_random_crop, \
-    my_normalize
+from Deep_Learning.kaggle.leaves.tools import crop_to_first, add_frame, distorted_input, load_images, batch_random_crop
 
 # settings
 LEAF_CROPS = 5
@@ -26,20 +25,20 @@ path_images = '/home/kamil/Documents/kaggle/leaves/images/'
 train_path = path + 'train.csv'
 test_path = path + 'test.csv'
 
-# 1. Load all images
+# Load all images
 all_images = load_images(path_images)
 
-# 2. Load all labels
+# Load all labels
 train = pd.read_csv(train_path)
 labels = train['species'].tolist()
 
-# 3. Create a dict of 'id': additional features
+# Create a dict of 'id': additional features
 additional = train.drop('species', 1).set_index('id').T.to_dict('list')
 
-# 4. Create a dict of 'id': species
+# Create a dict of 'id': species
 train_ids_species = train[['id', 'species']].set_index('id').T.to_dict('records')[0]
 
-# 5. Encode labels
+# Encode labels
 label_encoder = LabelEncoder()
 label_encoder.fit(labels)
 labels_int = label_encoder.transform(labels)
@@ -48,18 +47,19 @@ oh_encoder = OneHotEncoder()
 onehot_labels = oh_encoder.fit_transform([[x] for x in labels_int])
 onehot_labels = onehot_labels.toarray()  # array of one-hot encodings for each label
 
-# 6. Crop all images to the first occurence of a white pixel
+# Crop all images to the first occurence of a white pixel
 all_images = [crop_to_first(np.asarray(img)) for img in all_images]  # convert to array + crop
 
-# 7. Randomly crop all images using a square "filter"
+# Randomly crop all images using a square "filter"
 # 1st dim = a leaf; 2nd dim = one of LEAF_CROPS crops of the leaf; 3rd, 4th dim = image
 all_images_randomly_cropped = [y for x in all_images for y in [batch_random_crop(x, LEAF_CROPS)]]
 
 # TODO: Randomly distort images
+distorted_images = np.asarray([[distorted_input(x, 1) for x in x] for x in all_images_randomly_cropped])
 
+# TODO: Resize all images
 
 # TODO: Create a dict of images and their labels (w/ distorted images) 'id': [images]
-
 
 # TODO: uncomment it
 # Normalize all images
