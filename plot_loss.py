188de3e2da08ac38ea8a6d164b8c3e47af77b3d1
dydokamil <dydokamil@gmail.com>
2016-12-19 18:09:50 +0100

    Added plot smoothing

--- plot_loss.py
+++ plot_loss.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import spline
 
 from Deep_Learning.kaggle.leaves.tools import NB_EPOCH
 
@@ -13,12 +14,16 @@ all_data_list[0][0] = all_data_list[0][0][1:]
 all_data_list[-1][-1] = all_data_list[-1][-1][:-1]
 all_data_list = np.array(all_data_list, dtype=np.float64)
 
-losses = all_data_list[:, 0]
-accuracy = all_data_list[:, 1]
-x = np.arange(0, len(all_data_list)) * NB_EPOCH
+loss = np.array(all_data_list[:, 0])
+accuracy = np.array(all_data_list[:, 1])
 
-# plt.plot(x, losses, color='r')
-plt.plot(x, accuracy, color='g')
+x = np.arange(0, len(all_data_list)) * NB_EPOCH
+x_smooth = np.linspace(x.min(), x.max(), 100)
+accuracy_smooth = spline(x, accuracy, x_smooth)
+loss_smooth = spline(x, loss, x_smooth)
+
+plt.plot(x_smooth, loss_smooth, color='r')
+plt.plot(x_smooth, accuracy_smooth, color='g')
 plt.show()
 
 pass
