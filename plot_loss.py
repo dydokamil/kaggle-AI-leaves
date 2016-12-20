ff2441e9cd2f248528955ef3740e04fa82e8db0f
dydokamil <dydokamil@gmail.com>
2016-12-20 20:43:10 +0100

    Added legend to the plot, added the option to disable smoothing

--- plot_loss.py
+++ plot_loss.py
@@ -5,8 +5,10 @@ from scipy.interpolate import spline
 
 from Deep_Learning.kaggle.leaves.tools import NB_EPOCH
 
-with open("/media/kamil/c0a6bdfe-d860-4f81-8a6f-1f1d714ac49f/keras/leaves/losses2.txt", "r") as myfile:
+with open("/media/kamil/c0a6bdfe-d860-4f81-8a6f-1f1d714ac49f/keras/leaves/losses3.txt", "r") as myfile:
     all_data = myfile.read()
+
+SMOOTH = False
 
 all_data_list = all_data.split('][')
 all_data_list = [loss_acc.split(',') for loss_acc in all_data_list]
@@ -22,8 +24,14 @@ x_smooth = np.linspace(x.min(), x.max(), 100)
 accuracy_smooth = spline(x, accuracy, x_smooth)
 loss_smooth = spline(x, loss, x_smooth)
 
-plt.plot(x_smooth, loss_smooth, color='r')
-plt.plot(x_smooth, accuracy_smooth, color='g')
+if SMOOTH:
+    plt.plot(x_smooth, loss_smooth, color='r', label='loss')
+    plt.plot(x_smooth, accuracy_smooth, color='g', label='accuracy')
+else:
+    plt.plot(x, loss, color='r', label='loss')
+    plt.plot(x, accuracy, color='g', label='accuracy')
+
+plt.legend(loc='upper right')
 plt.show()
 
 pass
